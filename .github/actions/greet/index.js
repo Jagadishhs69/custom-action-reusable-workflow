d729399673fc3f3aed08ed76d7a9fa5f0b2fefc0
Jagadishhs69 <157796587+Jagadishhs69@users.noreply.github.com>
2025-06-12 13:02:31 +0530

    Delete .github/actions/greet directory

--- .github/actions/greet/index.js
+++
@@ -1,9 +0,0 @@
-const core = require('@actions/core');
-try {
-  const name = core.getInput('name');
-  console.log(`Hello, ${name}!`);
-  const timestamp = new Date().toISOString();
-  core.setOutput('timestamp', timestamp);
-} catch (error) {
-  core.setFailed(error.message);
-}
